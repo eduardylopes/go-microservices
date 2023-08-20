5780278d6db3752c71f406b040528c7c93a40108
Eduardy Lopes <eduardylopes@gmail.com>
2023-08-20 19:27:12 -0300

    feat(auth-service): modify status code response

--- authentication-service/cmd/api/handler.go
+++ authentication-service/cmd/api/handler.go
@@ -39,13 +39,13 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
 	if err != nil {
-		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
 		return
 	}
 
 	valid, err := user.PasswordMatches(requestPayload.Password)
 	if err != nil || !valid {
-		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
 		return
 	}
 
