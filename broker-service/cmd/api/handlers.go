d1d3165b913544e995e8a779838783b0685bbddf
Eduardy Lopes <eduardylopes@gmail.com>
2023-08-19 22:46:50 -0300

    feat(broker-file): upload handlers with helpers

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
-	payload := jsonResponse{
+	payload := JSONResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload, nil)
 }
