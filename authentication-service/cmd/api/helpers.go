d7dc9d0df590cf94da15c9328d209c281048f138
Eduardy Lopes <eduardylopes@gmail.com>
2023-08-20 19:27:49 -0300

    fix(auth-service): fix status code error

--- authentication-service/cmd/api/helpers.go
+++ authentication-service/cmd/api/helpers.go
@@ -63,6 +63,8 @@ func (app *Config) errorJSON(w http.ResponseWriter, err error, status int) error
 
 	if status == 0 {
 		statusCode = http.StatusBadRequest
+	} else {
+		statusCode = status
 	}
 
 	payload := JSONResponse{
