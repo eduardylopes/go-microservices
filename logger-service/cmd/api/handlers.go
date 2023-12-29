10e5e11d2f94f1506ac5dd9c2c3b04d355ca1139
Eduardy Lopes <eduardylopes@gmail.com>
2023-12-29 15:03:13 -0300

    feat(logger-service): deal with readJSON error

--- logger-service/cmd/api/handlers.go
+++ logger-service/cmd/api/handlers.go
@@ -13,14 +13,18 @@ type JSONPayload struct {
 
 func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
 	var requestPayload JSONPayload
-	app.readJSON(w, r, &requestPayload)
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
 
 	event := data.LogEntry{
 		Name: requestPayload.Name,
 		Data: requestPayload.Data,
 	}
 
-	err := app.Models.LogEntry.Insert(event)
+	err = app.Models.LogEntry.Insert(event)
 	if err != nil {
 		app.errorJSON(w, err, http.StatusBadRequest)
 		return
