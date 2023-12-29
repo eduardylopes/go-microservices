e5be99a13fc5eaeb3a620d27eb9524cc8f85ce2d
Eduardy Lopes <eduardylopes@gmail.com>
2023-12-28 23:51:47 -0300

    feat(broker-service): integrate with log service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +43,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unknown action"), http.StatusBadRequest)
 	}
@@ -83,6 +91,43 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		Error:   false,
 		Message: "authenticated",
 		Data:    JSONFromService.Data,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload, nil)
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	JSONData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	req, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(JSONData))
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	payload := JSONResponse{
+		Error:   false,
+		Message: "logged",
 	}
 
 	app.writeJSON(w, http.StatusAccepted, payload, nil)
